f57d083a9e13e15a70077719dc687a2d1fbe21c8
Jared H Liu <jared.h.liu@benq.com>
2018-11-03 10:33:09 +0800

    export ffmpeg

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'jaredhliu:ffmpeg',
   summary: 'ffmpeg packaged for meteor. Uses fluent-ffmpeg',
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/jaredliu2018/meteor-ffmpeg.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('ffmpeg.js');
+  api.export('ffmpeg');
 });
 
 Package.onTest(function(api) {
